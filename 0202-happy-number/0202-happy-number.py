772cce9c6cd3b44e9ebe8166a3bef5abf87dc7b8
Mohd Saif <saifofficial0778@gmail.com>
2026-07-30 11:25:50 +0530

    Time: 1 ms (47.07%), Space: 19.2 MB (61.38%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,13 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen=set()
-        while n!=1 and n not in seen:
-            seen.add(n)
+        def get_next(n):
             sum=0
             while n>0:
                 ld=n%10
                 sum+=ld*ld
                 n=n//10
-            n=sum
-        return n==1
-
+            return sum
+        slow=n
+        fast=get_next(n)
+        while fast!=1 and slow!=fast:
+            slow=get_next(slow)
+            fast=get_next(get_next(fast))
+        return fast==1
+        
