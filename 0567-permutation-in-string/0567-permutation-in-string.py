4346797767446c868a23492d7cfc067708f0495c
Mohd Saif <saifofficial0778@gmail.com>
2026-08-06 10:59:25 +0530

    Time: 23 ms (48.18%), Space: 19.3 MB (86.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,30 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         mydict={}
-        n=len(s1)
-        left=0
-        for i in range(0,n):
+        for i in range(0,len(s1)):
             if s1[i] not in mydict:
                 mydict[s1[i]]=1
             else:
                 mydict[s1[i]]+=1
         temp={}
-        for right in range(len(s2)):
+        left=0
+        for right in range(0,len(s2)):
             if s2[right] not in temp:
                 temp[s2[right]]=1
             else:
                 temp[s2[right]]+=1
-            while right-left+1>n:
+            while right-left+1>len(s1):
                 temp[s2[left]]-=1
+
                 if temp[s2[left]]==0:
                     del temp[s2[left]]
                 left+=1
             if temp==mydict:
                 return True
         return False
-        
-                
-
-
-
-        
