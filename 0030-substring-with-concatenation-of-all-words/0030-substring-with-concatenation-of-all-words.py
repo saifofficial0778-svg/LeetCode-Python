aea64274221c98d5160772ac2f4463ccf927b775
Mohd Saif <saifofficial0778@gmail.com>
2026-08-08 11:03:07 +0530

    Time: 19 ms (93.85%), Space: 20.1 MB (14.16%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -9,15 +9,33 @@ class Solution:
     
         res=[]
         n=len(s)
-        m=len(words)*len(words[0])
-        for i in range(0,n-m+1):
+        word_len = len(words[0])
+        for offset in range(word_len):
             temp={}
-            for j in range(i,i+m,len(words[0])):
-                word=s[j:j+len(words[0])]
+            left = offset 
+            count = 0          
+            for right in range(offset,n,word_len):
+                word=s[right:right+word_len]
+                
+                if word not in mydict:
+                    temp={}
+                    left = right + word_len
+                    count = 0
+                    continue
                 if word not in temp:
                     temp[word]=1
                 else:
                     temp[word]+=1
-            if temp==mydict:
-                res.append(i)
+                count+=1
+                while temp[word] > mydict[word]:
+                    left_word = s[left:left + word_len]
+                    temp[left_word] -= 1
+                    count -= 1
+                    if temp[left_word]==0:
+                        del temp[left_word]
+                    left+=word_len
+                
+                if count==len(words):
+                    res.append(left)
+       
         return res
