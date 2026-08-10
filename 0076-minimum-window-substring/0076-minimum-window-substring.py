e3e599323aef798412322d2e41eda0449c86955d
Mohd Saif <saifofficial0778@gmail.com>
2026-08-10 21:53:44 +0530

    Time: 59 ms (84.68%), Space: 19.7 MB (73.63%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,37 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         mydict={}
-        n=len(t)
         left=0
-        matched=0       
+        matched=0
         min_len=float('inf')
-        for i in range(n):
+        for i in range(len(t)):
             if t[i] not in mydict:
                 mydict[t[i]]=1
             else:
                 mydict[t[i]]+=1
-        
         for right in range(len(s)):
             if s[right] in mydict:
                 mydict[s[right]]-=1
                 if mydict[s[right]]>=0:
                     matched+=1
-            while matched==len(t):
-                if (right-left+1)<min_len:
-                    min_len=right-left+1
-                    start=left
-                if s[left] in mydict:
-                    mydict[s[left]]+=1
-                    if mydict[s[left]]>0:
-                        matched-=1
-                left+=1
+                while matched==len(t):
+                    if (right-left+1)<min_len:
+                        min_len=right-left+1
+                        start=left
+                    if s[left] in mydict:
+                        mydict[s[left]]+=1
+                        if mydict[s[left]]>0:
+                            matched-=1
+                    left+=1
         if min_len==float('inf'):
             return ""
         return s[start:start+min_len]
 
-
-            
-
-
-        
-        
+                
