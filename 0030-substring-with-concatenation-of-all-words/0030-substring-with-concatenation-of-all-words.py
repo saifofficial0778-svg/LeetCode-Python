c249f29ab4aae793834a8a7e6c2917530277c56b
Mohd Saif <saifofficial0778@gmail.com>
2026-08-13 10:59:53 +0530

    Time: 15 ms (99.11%), Space: 19.9 MB (69.97%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,41 +1,43 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        mydict={}
-        for i in range(0,len(words)):
-            if words[i] not in mydict:
-                mydict[words[i]]=1
-            else:
-                mydict[words[i]]+=1
-    
+        n=len(words)
+        need={}
+        for word in words:
+            if word not in need:
+                need[word]=1
+            else: 
+                need[word]+=1
+        
         res=[]
         n=len(s)
-        word_len = len(words[0])
+        word_len=len(words[0])
         for offset in range(word_len):
             temp={}
             left = offset 
-            count = 0          
+            count = 0 
+
             for right in range(offset,n,word_len):
                 word=s[right:right+word_len]
-                
-                if word not in mydict:
+
+                if word not in need:
                     temp={}
-                    left = right + word_len
-                    count = 0
+                    count=0
+                    left=right+word_len
                     continue
                 if word not in temp:
                     temp[word]=1
                 else:
                     temp[word]+=1
                 count+=1
-                while temp[word] > mydict[word]:
+                while temp[word]>need[word]:
                     left_word = s[left:left + word_len]
-                    temp[left_word] -= 1
-                    count -= 1
+                    temp[left_word]-=1
+                    count-=1
                     if temp[left_word]==0:
                         del temp[left_word]
+
                     left+=word_len
-                
                 if count==len(words):
                     res.append(left)
-       
         return res
+                    
