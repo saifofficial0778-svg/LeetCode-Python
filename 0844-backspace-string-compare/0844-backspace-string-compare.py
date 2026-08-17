d5fbcee9795c3c443a80fe7fddea8876bb728b14
Mohd Saif <saifofficial0778@gmail.com>
2026-08-17 09:48:12 +0530

    Time: 0 ms (100%), Space: 19.3 MB (24.18%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -4,9 +4,7 @@ class Solution:
         p2=len(t)-1
         s_skip=0
         t_skip=0
-
         while p1>=0 or p2>=0:
-
             while p1>=0:
                 if s[p1]=='#':
                     s_skip+=1
@@ -25,14 +23,12 @@ class Solution:
                     p2-=1
                 else:
                     break
+
             if p1>=0 and p2>=0:
                 if s[p1]!=t[p2]:
                     return False
-            elif (p1>=0 and p2<0) or (p2>=0 and p1<0):
+            elif (p1>=0 and p2<0) or (p1<0 and p2>=0):
                 return False
             p1-=1
             p2-=1
         return True
-
-
-            
