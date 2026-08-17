6c6255d578313d27451cb932ec569b77abff6733
Mohd Saif <saifofficial0778@gmail.com>
2026-08-17 10:13:02 +0530

    Time: 63 ms (38.63%), Space: 22.1 MB (85.67%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,12 +3,15 @@ class Solution:
         max_len=0
         zero_count=0
         left=0
+
         for right in range(len(nums)):
             if nums[right]==0:
                 zero_count+=1
+
             while zero_count>k:
                 if nums[left]==0:
                     zero_count-=1
                 left+=1
-            max_len=max(max_len,right-left+1)  
+            max_len=max(max_len,right-left+1)
         return max_len
+        
