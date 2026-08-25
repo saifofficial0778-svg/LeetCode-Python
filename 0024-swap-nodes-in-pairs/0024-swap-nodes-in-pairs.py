d2792e8f8a02565920841d786a77a5c9e955ffa2
Mohd Saif <saifofficial0778@gmail.com>
2026-08-25 12:18:26 +0530

    Time: 0 ms (100%), Space: 19.1 MB (90.64%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -11,14 +11,13 @@ class Solution:
         dummy.next=head
         prev=dummy
         while prev.next and prev.next.next:
-            first =prev.next
+            first=prev.next
             second=first.next
 
             first.next=second.next
             second.next=first
             prev.next=second
             prev=first
-
         return dummy.next
 
         
