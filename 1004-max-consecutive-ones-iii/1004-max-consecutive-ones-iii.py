81dfe6be5bd6b36b2f45f79ad9b3513f9f81c2ef
Mohd Saif <saifofficial0778@gmail.com>
2026-09-01 12:19:40 +0530

    Time: 51 ms (85.57%), Space: 22.4 MB (6.34%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        max_len=0
         zero_count=0
+        max_len=0
         left=0
 
         for right in range(len(nums)):
@@ -12,6 +12,6 @@ class Solution:
                 if nums[left]==0:
                     zero_count-=1
                 left+=1
+
             max_len=max(max_len,right-left+1)
         return max_len
-        
