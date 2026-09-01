01da7057f78870b1ceaf8055250e905ac85865b6
Mohd Saif <saifofficial0778@gmail.com>
2026-09-01 12:21:39 +0530

    Time: 47 ms (90.33%), Space: 22 MB (98.21%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -12,6 +12,6 @@ class Solution:
                 if nums[left]==0:
                     zero_count-=1
                 left+=1
-
+            
             max_len=max(max_len,right-left+1)
         return max_len
