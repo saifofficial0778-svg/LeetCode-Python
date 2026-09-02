027303344e362d2ceb9f2b5c6ece53c7bdf686c7
Mohd Saif <saifofficial0778@gmail.com>
2026-09-02 23:35:02 +0530

    Time: 55 ms (90.8%), Space: 19.6 MB (94.32%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,17 +1,19 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         mydict={}
+        matched=0
+        left=0
+        min_len=float('inf')
         for i in range(len(t)):
             if t[i] not in mydict:
                 mydict[t[i]]=1
             else:
                 mydict[t[i]]+=1
-        matched=0
-        left=0
-        min_len=float('inf')
+
         for right in range(len(s)):
             if s[right] in mydict:
                 mydict[s[right]]-=1
+
                 if mydict[s[right]]>=0:
                     matched+=1
 
@@ -21,12 +23,13 @@ class Solution:
                     start=left
                 if s[left] in mydict:
                     mydict[s[left]]+=1
+
                     if mydict[s[left]]>0:
                         matched-=1
                 left+=1
         if min_len==float('inf'):
             return ""
         return s[start:start+min_len]
-
-
-
+        
+        
+        
