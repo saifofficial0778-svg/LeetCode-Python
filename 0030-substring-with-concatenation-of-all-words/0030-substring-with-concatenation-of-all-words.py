fb2c23522f57d6040bbe6a8a04dc666ef99e3ec6
Mohd Saif <saifofficial0778@gmail.com>
2026-09-06 11:00:01 +0530

    Time: 20 ms (84.89%), Space: 19.8 MB (69.73%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,43 +1,46 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        n=len(words)
-        need={}
+        mydict = {}
+        res = []
+
         for word in words:
-            if word not in need:
-                need[word]=1
-            else: 
-                need[word]+=1
-        
-        res=[]
-        n=len(s)
-        word_len=len(words[0])
+            mydict[word] = mydict.get(word, 0) + 1
+
+        word_len = len(words[0])
+        total_words = len(words)
+
         for offset in range(word_len):
-            temp={}
-            left = offset 
-            count = 0 
+            left = offset
+            temp = {}
+            count = 0
 
-            for right in range(offset,n,word_len):
-                word=s[right:right+word_len]
+            for right in range(offset, len(s), word_len):
+                word = s[right:right + word_len]
 
-                if word not in need:
-                    temp={}
-                    count=0
-                    left=right+word_len
+                # word doesn't exist in words
+                if word not in mydict:
+                    temp = {}
+                    count = 0
+                    left = right + word_len
                     continue
-                if word not in temp:
-                    temp[word]=1
-                else:
-                    temp[word]+=1
-                count+=1
-                while temp[word]>need[word]:
-                    left_word = s[left:left + word_len]
-                    temp[left_word]-=1
-                    count-=1
-                    if temp[left_word]==0:
-                        del temp[left_word]
 
-                    left+=word_len
-                if count==len(words):
+                temp[word] = temp.get(word, 0) + 1
+                count += 1
+
+                # too many copies of this word
+                while temp[word] > mydict[word]:
+                    word_left = s[left:left + word_len]
+
+                    temp[word_left] -= 1
+
+                    if temp[word_left] == 0:
+                        del temp[word_left]
+
+                    left += word_len
+                    count -= 1
+
+                # all words matched
+                if count == total_words:
                     res.append(left)
+
         return res
-                    
