0bf8c1588dc5f07f45765b4ce7ec7efd27df3291
Mohd Saif <saifofficial0778@gmail.com>
2026-09-07 11:19:01 +0530

    Time: 4 ms (14.97%), Space: 19.4 MB (6.71%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        freq={}
+
+        for ch in s:
+            freq[ch]=freq.get(ch,0)+1
+        
+        ans=0
+        odd=False
+
+        for count in freq.values():
+            ans+=(count//2)*2
+
+            if count%2==1:
+                odd=True
+
+        if odd:
+            ans+=1
+        return ans
