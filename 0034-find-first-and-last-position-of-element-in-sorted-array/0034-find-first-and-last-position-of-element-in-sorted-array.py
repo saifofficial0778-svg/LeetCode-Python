b942f23fbbda89771854ee9876f0c9c369ef0a8e
Mohd Saif <saifofficial0778@gmail.com>
2026-09-09 10:17:25 +0530

    Time: 0 ms (100%), Space: 20.6 MB (61.24%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,37 +1,36 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        fa=-1
-        la=-1
-        res=[]
-        n=len(nums)
-        low=0
-        high=n-1
-        low1=0
-        high1=n-1
+        n = len(nums)
 
-        while low<=high:
-            mid=(low+high)//2
-        
-            if nums[mid]>=target:
-                if nums[mid]==target:
-                    fa=mid
-                high=mid-1
-                
-            else:
-                low=mid+1
-        res.append(fa)
+        low1, high1 = 0, n - 1
+        low2, high2 = 0, n - 1
 
-        while low1<=high1:
-            mid1=(low1+high1)//2
-            if nums[mid1]<=target:
-                if nums[mid1]==target:
-                    la=mid1
-                low1=mid1+1
-                
-            else:
-                high1=mid1-1
-        res.append(la)
-        return res
+        first = -1
+        ceil = -1
 
         
+        while low1 <= high1:
+            mid1 = (low1 + high1) // 2
 
+            if nums[mid1] >= target:
+                first = mid1
+                high1 = mid1 - 1
+            else:
+                low1 = mid1 + 1
+
+        
+        while low2 <= high2:
+            mid2 = (low2 + high2) // 2
+
+            if nums[mid2] > target:
+                ceil = mid2
+                high2 = mid2 - 1
+            else:
+                low2 = mid2 + 1
+
+        if first == -1 or nums[first] != target:
+            return [-1, -1]
+        if ceil==-1:
+            return [first,n-1]
+
+        return [first, ceil - 1]
