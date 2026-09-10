f61581181f5f42c5624d0db995f36b3fe2ce88b5
Mohd Saif <saifofficial0778@gmail.com>
2026-09-10 10:59:06 +0530

    Time: 19 ms (63.77%), Space: 19.5 MB (72.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        freq={}
+        source = {}
+
         for ch in magazine:
-            freq[ch]=freq.get(ch,0)+1
+            source[ch] = source.get(ch, 0) + 1
 
         for ch in ransomNote:
-            if ch not in freq or freq[ch]==0:
+            if ch not in source or source[ch] == 0:
                 return False
-            freq[ch]-=1
+
+            source[ch] -= 1
+
         return True
