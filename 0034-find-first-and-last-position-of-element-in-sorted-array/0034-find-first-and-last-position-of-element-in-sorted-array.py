a60c984dce86d83bc214634a00b1bc95fdf24d6d
Mohd Saif <saifofficial0778@gmail.com>
2026-09-11 11:26:36 +0530

    Time: 0 ms (100%), Space: 20.6 MB (61.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -3,34 +3,30 @@ class Solution:
         n = len(nums)
 
         low1, high1 = 0, n - 1
+
         low2, high2 = 0, n - 1
 
-        first = -1
-        ceil = -1
+        first, last = -1, -1
 
-        
-        while low1 <= high1:
-            mid1 = (low1 + high1) // 2
+        while low1<=high1:
+            mid1=(low1+high1)//2
 
-            if nums[mid1] >= target:
-                first = mid1
-                high1 = mid1 - 1
+            if nums[mid1]>=target:
+                first=mid1
+                high1=mid1-1
             else:
-                low1 = mid1 + 1
+                low1=mid1+1
+        while low2<=high2:
+            mid2=(low2+high2)//2
 
-        
-        while low2 <= high2:
-            mid2 = (low2 + high2) // 2
-
-            if nums[mid2] > target:
-                ceil = mid2
-                high2 = mid2 - 1
+            if nums[mid2]>target:
+                last=mid2
+                high2=mid2-1
             else:
-                low2 = mid2 + 1
-
-        if first == -1 or nums[first] != target:
-            return [-1, -1]
-        if ceil==-1:
+                low2=mid2+1
+        if first==-1 or nums[first]!=target:
+            return [-1,-1]
+        if last==-1:
             return [first,n-1]
-
-        return [first, ceil - 1]
+        return [first,last-1]
+            
