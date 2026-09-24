ce34556866297d6b78053671db2b2080a498767b
Mohd Saif <saifofficial0778@gmail.com>
2026-09-24 09:25:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.7%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,18 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        freq={}
-
+        mydict={}
         for ch in s:
-            freq[ch]=freq.get(ch,0)+1
+            mydict[ch]=mydict.get(ch,0)+1
 
         odd=False
         ans=0
 
-        for count in freq.values():
-            ans+=(count//2)*2
-            if count%2==1:
+        for value in mydict.values():
+            ans+=(value//2)*2
+            if value%2==1:
                 odd=True
-        
         if odd:
             ans+=1
         return ans
